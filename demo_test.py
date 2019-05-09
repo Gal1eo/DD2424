6bcd242adb628496f2562eb47f45b25aa8eb2e9e
Gal1eo <roylandstinger@gmail.com>
2019-05-09 22:28:35 +0200

    demo test

---
+++ demo_test.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import csv
+import os
+import shutil
+import logging
+import argparse
+import random
+from tqdm import tqdm, trange
+import json
+
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+from pytorch_pretrained_bert.modeling import BertForMaskedLM
+from pytorch_pretrained_bert.optimization import BertAdam
+from pytorch_pretrained_bert.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
+
+# Load fine-tuned bertmask model
+def load_model(model_name):
+    weights_path = os.path.join(PYTORCH_PRETRAINED_BERT_CACHE, model_name)
+    model = torch.load(weights_path)
+    return model
+
+cbert_name = "{}/BertForMaskedLM_{}_epoch_10".format("toxic", "toxic")
+model = load_model(cbert_name)
+model.cuda()
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased", do_lower_case=True)
+
+text = "dummy. you are a loser"
+target = "loser"
+tokenized_text = tokenizer.tokenize(text)
+
+# Mask a token that we will try to predict back with `BertForMaskedLM`
+masked_index = tokenized_text.index(target)
+tokenized_text[masked_index] = '[MASK]'
+
+# Convert token to vocabulary indices
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+# Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
+segments_ids = [1] * len(tokenized_text)
+# this is for the dummy first sentence.
+segments_ids[0] = 0
+segments_ids[1] = 0
+
+# Convert inputs to PyTorch tensors
+tokens_tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
+# Load pre-trained model (weights)
+model = BertForMaskedLM.from_pretrained("bert-base-uncased")
+model.eval()
+
+# Predict all tokens
+predictions = model(tokens_tensor, segments_tensors)
+predicted_index = torch.argmax(predictions[0, masked_index]).item()
+predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])
+
+print("Original:", text)
+print("Masked:", " ".join(tokenized_text))
+
+print("Predicted token:", predicted_token)
+print("Other options:")
+# just curious about what the next few options look like.
+for i in range(10):
+    predictions[0,masked_index,predicted_index] = -11100000
+    predicted_index = torch.argmax(predictions[0, masked_index]).item()
+    predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])
+    print(predicted_token)
