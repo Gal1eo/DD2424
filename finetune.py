e5117284c419bee8f07d648dc6e585acd3e49f62
Gal1eo <roylandstinger@gmail.com>
2019-05-09 16:26:52 +0200

    Modify Create examples

--- finetune.py
+++ finetune.py
@@ -83,7 +83,7 @@ class AugProcessor(DataProcessor):
     def get_train_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_csv(os.path.join(data_dir, "train.tsv")), "train")
+            self._read_csv(os.path.join(data_dir, "train.csv")), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
