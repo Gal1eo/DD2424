177fa281cacc0411ede24269841414b61de897c1
Gal1eo <roylandstinger@gmail.com>
2019-05-10 15:48:46 +0200

    demo test

--- demo_test.py
+++ demo_test.py
@@ -49,7 +49,7 @@ segments_ids[0] = 0
 segments_ids[1] = 0
 
 # Convert inputs to PyTorch tensors
-torch.tensor.to('cuda')
+torch.tensor = torch.tensor.to('cuda')
 
 tokens_tensor = torch.tensor([indexed_tokens])
 segments_tensors = torch.tensor([segments_ids])
